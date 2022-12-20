2cde9a32f6c88c94f074402c227615af6000ac14
AlxGrshn <113042128+AlxGrshn@users.noreply.github.com>
2022-12-20 18:22:57 +0300

    Initial Commit

---
+++ SliderScore/SliderScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderScoreApp.swift
+//  SliderScore
+//
+//  Created by Alexander Grishin on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
